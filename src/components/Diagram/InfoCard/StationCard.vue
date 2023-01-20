5be551c15f01b338cc08a0e08e1f5a02d04e350b
Carl Kwok <carlkwokcy@gmail.com>
2023-01-20 17:05:03 +0800

    feat: add redirection link on clicking line symbol

--- src/components/Diagram/InfoCard/StationCard.vue
+++ src/components/Diagram/InfoCard/StationCard.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
-div 
-	h4.station-name {{ data?.station?.norwegian }}
+#station-card
+	h2.station-name.pb-1 {{ data?.station?.norwegian }}
 	v-chip.line-info.mr-2(
 		v-if="data?.line"
 		v-for="line in data.line"
 		:key="line.id"
 		:color="getColor(line)"
-		label small) 
+		label small
+		:to="redirect(line.id)") 
 		.white--text.font-weight-black {{ line?.code }}
-	h6.fare-zone-info(v-if="data?.zone")
-		v-icon(x-small) mdi-currency-usd 
+	h4.fare-zone-info.mt-2(v-if="data?.zone")
+		v-icon(small) mdi-currency-usd 
 		| {{ data?.zone?.norwegian }} 
 		span(v-if="data?.zone?.english") ({{ data.zone?.english }})
 </template>
@@ -22,10 +23,10 @@ export default {
 	methods: {
 		getColor(lineInfo) {
 			return `#${lineInfo.color}`;
+		},
+		redirect(lineId) {
+			return `/line?lineId=${lineId}`;
 		}
-	},
-	mounted() {
-		console.log("StationCard", this.data);
 	}
 };
 </script>
